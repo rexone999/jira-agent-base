f6b99ae6718daadb82cbcfa35e14a850b6213add
rexone999 <projectmajorvd25@gmail.com>
2025-06-08 22:54:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+const path = require('path');
 
+app.get('/atlassian-connect.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'atlassian-connect.json'));
+});
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
